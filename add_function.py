36ecb6e280556ea2397afea21ec9b6580211dc20
pgscastro <33170789+pgscastro@users.noreply.github.com>
2024-06-11 11:32:25 -0700

    Improvments
    Improved adding, remove functions.
    now it's possible to input dictionary with tasklist with deadline and date

--- add_function.py
+++ add_function.py
@@ -3,17 +3,14 @@
 import pandas as pd
 
 
-def task(tdl):
-
+def task(df):
     # user inputs strings to the add_task
     add_task = input("Please add a task to the list: ")
     add_priority = input("Please choose a priority between 1-10: ")
     add_date = input("Please add the deadline (YYYY/MM/DD): ")
     print(f"Awesome! you added {add_task} with {add_priority} due {add_date} to your to-do list")
-    # storaging the add_task to thee list called "task_list"
-    # adding a add_task to the general list task_list
-    df = pd.DataFrame({"task": [add_task], "priority": [add_priority], "date": [add_date]})
-    print(df)
-    tdl.append(add_task)
-
-    return df
+    # storaging the add_task to the list called "task_list"
+    # adding add_task to the general list task_list
+    df_new = pd.DataFrame({"Task": [add_task], "Priority": [add_priority], "Deadline": [add_date]})
+    df_new = pd.concat([df, df_new], ignore_index=True)
+    return df_new
--- remove_function.py
+++ remove_function.py
@@ -1,8 +1,19 @@
 # Remove a task from the to-do list.
+import inquirer
+
 
 def remove_tasks(tdl):
-    remove1 = input("Write a task you want to remove: ")
-    print(remove1)
-    tdl.remove(remove1)
+    task_list = tdl["Task"].tolist()
+    questions = [
+        inquirer.Checkbox('choice',
+                          message="To do list",
+                          choices=task_list,
+                          ),
+    ]
+    answers = inquirer.prompt(questions)
+    for i in answers["choice"]:
+        tdl.drop(tdl[tdl["Task"] == i].index, inplace=True)
+        print(i)
     print(tdl)
+
     return tdl
