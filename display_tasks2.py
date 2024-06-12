4e4bc6773d9a079f52fc30b764cfcc6a82391051
pamkmoll <pamkmoll@gmail.com>
2024-06-11 23:09:04 -0700

    Add print to csv feature and display tasks with sorting deadline and importance.

--- display_tasks2.py
+++ display_tasks2.py
@@ -1,4 +1,12 @@
-def print_TDL (list):
-    for i in range(len(list)):
-        print(list[i])
-#testing branch
+import pandas as pd
+
+def print_TDL(tdl, sort_by=None):
+    # dataframe for sorting
+    df = pd.DataFrame(tdl)
+
+    if sort_by == 'priority':
+        df = df.sort_values(by='priority')
+    elif sort_by == 'date':
+        df = df.sort_values(by='date')
+    print(df)
+    
